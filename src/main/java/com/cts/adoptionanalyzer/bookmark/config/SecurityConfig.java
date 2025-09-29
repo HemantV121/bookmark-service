3102ce1e773ed78e09ca3387d6958cd3c0bb719d
zadmin <zadmin@vm-lab133-13-430.asia-south1-a.c.niit-stackroute-2.internal>
2025-09-29 17:26:47 +0530

    updated code

---
+++ src/main/java/com/cts/adoptionanalyzer/bookmark/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.cts.adoptionanalyzer.bookmark.config;
+import com.cts.adoptionanalyzer.bookmark.security.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain configureSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(
+                        request -> request.requestMatchers("/**").hasAnyRole("USER")
+                                .anyRequest().authenticated()
+                )
+                .csrf(c -> c.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+
+}
